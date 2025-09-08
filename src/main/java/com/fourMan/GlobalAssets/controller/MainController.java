6abc3469733c057e1447c10f4d9dbe59556579b1
변지환 <ghks0115@gmail.com>
2025-09-08 22:20:07 +0900

    html뼈대추가

--- src/main/java/com/fourMan/GlobalAssets/controller/MainController.java
+++ src/main/java/com/fourMan/GlobalAssets/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.fourMan.GlobalAssets.controller;
 
+import com.fourMan.GlobalAssets.dto.UserDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,5 +10,19 @@ public class MainController {
     @GetMapping({"/",""})
     public String GotoMain(){
         return "main";
+    }
+
+    @GetMapping("login")
+    public String GotoLogin(Model model){
+        UserDto dto = new UserDto();
+        model.addAttribute("dto", dto);
+        return "login";
+    }
+
+    @GetMapping("signup")
+    public String GotoSignup(Model model){
+        UserDto dto = new UserDto();
+        model.addAttribute("dto", dto);
+        return "signup";
     }
 }
